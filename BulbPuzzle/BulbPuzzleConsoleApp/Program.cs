1135ab8a996c4b8fe49004c9f5c120372b760aa8
Ernie Cruz <ernie.cruz@umusic.com>
2017-08-21 22:18:40 -0700

    Move and reorganize
    Changed the name of some folders and moved the solution file into its
    own folder.

---
+++ BulbPuzzle/BulbPuzzleConsoleApp/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BulbPuzzleLibrary;
+
+namespace BulbPuzzleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = 100;
+            int bulbs = 100;
+
+            RunPuzzle(people, bulbs);
+#if DEBUG
+            Console.WriteLine("Press <Enter> to exit.");
+            Console.ReadLine();
+#endif
+        }
+
+        private static void RunPuzzle(int numberOfPeople, int numberOfBulbs)
+        {
+            Console.WriteLine("New puzzle run with {0} people and {1} bulbs.", numberOfPeople, numberOfBulbs);
+
+            BulbPuzzle puzzle = new BulbPuzzle(numberOfPeople, numberOfBulbs);
+            int numberOfBulbsOn = puzzle.Run();
+            Console.WriteLine("Bulbs on: {0}", numberOfBulbsOn);
+            bool[] bulbs = puzzle.Bulbs;
+
+            Console.WriteLine("Bulbs turned on are:");
+            int counter = 1;
+            for (int i = 0; i < puzzle.Bulbs.Length; i++)
+            {
+                if (bulbs[i])
+                {
+                    Console.WriteLine("{0,2}) {1}", counter++, i + 1);
+                }
+            }
+        }
+    }
+}
