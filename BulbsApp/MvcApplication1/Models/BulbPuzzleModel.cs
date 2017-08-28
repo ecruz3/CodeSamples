3227adda7750a0e0eef0e0a100e09cb89493d78e
Ernie Cruz <ernie.cruz@umusic.com>
2017-08-27 18:18:27 -0700

    Change view
    Use the layout page. Display the bulbs that are on.

--- BulbsApp/MvcApplication1/Models/BulbPuzzleModel.cs
+++ BulbsApp/MvcApplication1/Models/BulbPuzzleModel.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel;
 using System.Linq;
+using System.Text;
 using System.Web;
 using BulbPuzzleLibrary;
+
 namespace MvcApplication1.Models
 {
     public class BulbPuzzleModel
@@ -27,6 +29,28 @@ namespace MvcApplication1.Models
             BulbPuzzle puzzle = new BulbPuzzle(NumberOfPeople, NumberOfBulbs);
             _countOfBulbsOn = puzzle.Run();
             _bulbs = puzzle.Bulbs;
+        }
+
+        public string BulbsOn
+        {
+            get
+            {
+                StringBuilder sb = new StringBuilder();
+                int onBulbCounter = 0;
+                for (int i = 0; i < _bulbs.Length; i++)
+                {
+                    if (_bulbs[i])
+                    {
+                        sb.Append(i + 1);
+                        onBulbCounter++;
+                        if (onBulbCounter < _countOfBulbsOn)
+                        {
+                            sb.Append(", ");
+                        }
+                    }
+                }
+                return sb.ToString();
+            }
         }
     }
 }
